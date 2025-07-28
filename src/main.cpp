9873be01f6f25930cf1326afb0485f9d8733b9b0
Hyarius <Hyarius@erelia-studio.com>
2025-07-29 01:11:17 +0200

    Working on creating the structure for player control

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <sparkle.hpp>
-#include <spk_debug_macro.hpp>
 
 enum class Event
 {
@@ -8,7 +7,7 @@ enum class Event
 
 using EventDispatcher = spk::EventDispatcher<Event>;
 
-class Controller : public spk::Component
+class CommandReceiver : public spk::Component
 {
 public:
 	enum class ActionType
@@ -24,31 +23,10 @@ private:
 
 	std::unordered_map<ActionType, std::unique_ptr<spk::Action>> _actions;
 
-	spk::OpenGL::UniformBufferObject& _cameraUBO;
-
-	static spk::OpenGL::UniformBufferObject& requestCameraUBO()
-	{
-		if (spk::Lumina::Shader::Constants::containsUBO(L"cameraUBO") == false)
-		{
-			spk::OpenGL::UniformBufferObject newUBO = spk::OpenGL::UniformBufferObject(L"CameraUBO", 0, 128);
-
-			newUBO.addElement(L"viewMatrix", 0, 64);
-			newUBO.addElement(L"projectionMatrix", 0, 64);
-
-			spk::Lumina::Shader::Constants::addUBO(L"cameraUBO", std::move(newUBO));
-		}
-		return (spk::Lumina::Shader::Constants::ubo(L"cameraUBO"));
-	}
-
 	spk::SafePointer<spk::KeyboardAction> _moveUpAction;
 	spk::SafePointer<spk::KeyboardAction> _moveDownAction;
 	spk::SafePointer<spk::KeyboardAction> _moveRightAction;
 	spk::SafePointer<spk::KeyboardAction> _moveLeftAction;
-
-	spk::Camera _camera;
-	spk::GameObject _cameraHolder;
-
-	spk::ContractProvider::Contract _onEditionCallback;
 
 protected:
 	template <typename Enum>
@@ -59,29 +37,10 @@ protected:
 		return (result);
 	}
 
-	void onUpdateEvent(spk::UpdateEvent &p_event)
-	{
-		
-		for (auto &[key, action] : _actions)
-		{
-			if (action->isInitialized() == false)
-			{
-				action->initialize(p_event);
-			}
-
-			action->update();
-		}
-	}
-
 public:
-	Controller(const std::wstring &name) :
-		spk::Component(name),
-		_cameraHolder(L"CameraHolder", nullptr),
-		_cameraUBO(requestCameraUBO())
+	CommandReceiver(const std::wstring &name) :
+		spk::Component(name)
 	{
-		_cameraHolder.transform().place({0, 0, 10});
-		_cameraHolder.transform().lookAt({0, 0, 0});
-
 		_moveUpAction = setAction(ActionType::MoveUp, std::make_unique<spk::KeyboardAction>(spk::Keyboard::Z, spk::InputState::Down, 26,
 			[&](const spk::SafePointer<const spk::Keyboard>& p_keyboard) {
 				EventDispatcher::instance()->emit(Event::MovePlayer, spk::Vector3(0, 1, 0));
@@ -103,7 +62,55 @@ public:
 			})).upCast<spk::KeyboardAction>();
 	}
 
-	void awake()
+	void onUpdateEvent(spk::UpdateEvent &p_event) override
+	{
+		for (auto &[key, action] : _actions)
+		{
+			if (action->isInitialized() == false)
+			{
+				action->initialize(p_event);
+			}
+
+			action->update();
+		}
+	}
+};
+
+class TopDownCamera : public spk::Component
+{
+private:
+	spk::OpenGL::UniformBufferObject& _cameraUBO;
+
+	static spk::OpenGL::UniformBufferObject& requestCameraUBO()
+	{
+		if (spk::Lumina::Shader::Constants::containsUBO(L"cameraUBO") == false)
+		{
+			spk::OpenGL::UniformBufferObject newUBO = spk::OpenGL::UniformBufferObject(L"CameraUBO", 0, 128);
+
+			newUBO.addElement(L"viewMatrix", 0, 64);
+			newUBO.addElement(L"projectionMatrix", 0, 64);
+
+			spk::Lumina::Shader::Constants::addUBO(L"cameraUBO", std::move(newUBO));
+		}
+		return (spk::Lumina::Shader::Constants::ubo(L"cameraUBO"));
+	}
+
+	spk::Camera _camera;
+	spk::GameObject _cameraHolder;
+
+	spk::ContractProvider::Contract _onEditionCallback;
+
+public:
+	CommandReceiver(const std::wstring &name) :
+		spk::Component(name),
+		_cameraHolder(L"CameraHolder", nullptr),
+		_cameraUBO(requestCameraUBO())
+	{
+		_cameraHolder.transform().place({0, 0, 10});
+		_cameraHolder.transform().lookAt({0, 0, 0});
+	}
+
+	void awake() override
 	{
 		owner()->addChild(&_cameraHolder);
 
@@ -125,17 +132,37 @@ public:
 	}
 };
 
+class Updater : public spk::Component
+{
+private:
+
+public:
+	Updater(const std::wstring &name) :
+		spk::Component(name)
+	{
+		
+	}
+
+	void awake() override
+	{
+
+	}
+};
+
 class Player : public spk::GameObject
 {
 private:
-	Controller& _controller;
+	CommandReceiver& _controller;
+	TopDownCamera& _topDownCamera;
 
 public:
 	Player(const std::wstring &p_name, spk::SafePointer<spk::GameObject> p_parent) :
 		spk::GameObject(p_name, p_parent),
-		_controller(addComponent<Controller>(L"Controller"))
+		_controller(addComponent<CommandReceiver>(L"CommandReceiver")),
+		_topDownCamera(addComponent<TopDownCamera>(L"TopDownCamera"))
 	{
 		_controller.activate();
+		_topDownCamera.activate();
 	}
 };
 
@@ -156,11 +183,22 @@ struct PlayerManager : public spk::GameObject
 private:
 	EventDispatcher::Instanciator _eventDispatcherInstanciator;
 
+	spk::TContractProvider<spk::Vector3>::Contract _onMotionRequestContract;
+
+	spk::SafePointer<Player> _player;
+
 public:
 	PlayerManager(const std::wstring &p_name, spk::SafePointer<spk::GameObject> p_parent) :
 		spk::GameObject(p_name, p_parent)
 	{
+		_onMotionRequestContract = EventDispatcher::instance()->subscribe(Event::MovePlayer, [&](const spk::Vector3& p_delta){
+			p_player->move()
+		});
+	}
 
+	void bindPlayer(const spk::SafePointer<Player>& p_player)
+	{
+		_player = p_player;
 	}
 };
 
@@ -169,18 +207,23 @@ struct Context
 	spk::GameEngine engine;
 
 	Player player;
+	PlayerManager playerManager;
 	World world;
 
 	Context() :
 		player(L"Player", nullptr),
-		world(L"World", nullptr)
+		world(L"World", nullptr),
+		playerManager(L"PlayerManager", nullptr)
 	{
 		player.transform().place({0, 0, 0});
 		player.activate();
 
+		playerManager.activate();
+
 		world.activate();
 		
 		engine.addEntity(&player);
+		engine.addEntity(&playerManager);
 		engine.addEntity(&world);
 	}
 };
