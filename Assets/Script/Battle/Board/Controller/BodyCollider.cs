51f17b382e33a16f7096c436b5106f2f61e92b2e
Hyarius@erelia-studio.com <Hyarius@erelia-studio.com>
2026-02-12 11:40:52 +0100

    Activatting the board collision mesh

--- Assets/Script/Battle/Board/Controller/BodyCollider.cs
+++ Assets/Script/Battle/Board/Controller/BodyCollider.cs
@@ -5,12 +5,10 @@ namespace Battle.Board.Controller
 	public class BodyCollider : MonoBehaviour
 	{
 		[SerializeField] private Battle.Board.Controller.VoxelCollider voxelCollider = null;
-		[SerializeField] private Battle.Board.Controller.MaskCollider maskCollider = null;
 
 		private void Awake()
 		{
 			InitializeSolidCollider();
-			InitializeMaskCollider();
 		}
 
 		private void InitializeSolidCollider()
@@ -20,17 +18,9 @@ namespace Battle.Board.Controller
 			voxelCollider = go.AddComponent<Battle.Board.Controller.VoxelCollider>();
 		}
 
-		private void InitializeMaskCollider()
-		{
-			var go = new GameObject("BushChunkCollider");
-			go.transform.SetParent(transform, false);
-			maskCollider = go.AddComponent<Battle.Board.Controller.MaskCollider>();
-		}
-
 		public void Rebuild(Battle.Board.Model.Data data)
 		{
-			// voxelCollider.Rebuild(data);
-			// maskCollider.Rebuild(data);
+			voxelCollider.Rebuild(data);
 		}
 	}
 }
