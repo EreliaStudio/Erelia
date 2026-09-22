5dbd27f8e7714eb25c983ebac0ae94006d81ea9c
Hyarius <hyarius@erelia-studio.com>
2026-09-22 23:35:25 +0200

    refactor: move voxel cell implementation to source

--- core/include/erelia/core/voxel/cell.hpp
+++ core/include/erelia/core/voxel/cell.hpp
@@ -25,48 +25,21 @@ namespace Voxel
 		};
 
 	private:
-		inline static constexpr std::uint32_t DefinitionMask = 0x1FFFFFFFu;
-		inline static constexpr std::uint32_t OrientationMask = 0x60000000u;
-		inline static constexpr std::uint32_t FlipOrientationMask = 0x80000000u;
-		inline static constexpr std::uint32_t OrientationShift = 29u;
-		inline static constexpr std::uint32_t FlipOrientationShift = 31u;
-
-		std::uint32_t _packed = 0;
+		std::uint32_t _packed;
 
 	public:
 		static const Cell Empty;
 
-		Cell() noexcept = default;
-		explicit Cell(std::uint32_t packed) noexcept :
-			_packed(packed)
-		{
-		}
-
+		Cell() noexcept;
+		explicit Cell(std::uint32_t packed) noexcept;
 		Cell(
 			std::uint32_t definitionId,
 			Orientation orientation,
 			FlipOrientation flipOrientation);
 
-		[[nodiscard]] std::uint32_t definitionId() const noexcept
-		{
-			return _packed & DefinitionMask;
-		}
-
-		[[nodiscard]] Orientation orientation() const noexcept
-		{
-			return static_cast<Orientation>(
-				(_packed & OrientationMask) >> OrientationShift);
-		}
-
-		[[nodiscard]] FlipOrientation flipOrientation() const noexcept
-		{
-			return static_cast<FlipOrientation>(
-				(_packed & FlipOrientationMask) >> FlipOrientationShift);
-		}
-
-		[[nodiscard]] std::uint32_t packed() const noexcept
-		{
-			return _packed;
-		}
+		[[nodiscard]] std::uint32_t definitionId() const noexcept;
+		[[nodiscard]] Orientation orientation() const noexcept;
+		[[nodiscard]] FlipOrientation flipOrientation() const noexcept;
+		[[nodiscard]] std::uint32_t packed() const noexcept;
 	};
 }
