d0e67dcfed94bc61d35e9573081bbe94b3667f80
Hyarius <hyarius@erelia-studio.com>
2026-09-22 23:35:26 +0200

    refactor: move voxel cell implementation to source

--- core/src/voxel/cell.cpp
+++ core/src/voxel/cell.cpp
@@ -2,9 +2,28 @@
 
 #include <exception.hpp>
 
+namespace
+{
+	constexpr std::uint32_t DefinitionMask = 0x1FFFFFFFu;
+	constexpr std::uint32_t OrientationMask = 0x60000000u;
+	constexpr std::uint32_t FlipOrientationMask = 0x80000000u;
+	constexpr std::uint32_t OrientationShift = 29u;
+	constexpr std::uint32_t FlipOrientationShift = 31u;
+}
+
 namespace Voxel
 {
 	const Cell Cell::Empty{};
+
+	Cell::Cell() noexcept :
+		_packed(0)
+	{
+	}
+
+	Cell::Cell(std::uint32_t packed) noexcept :
+		_packed(packed)
+	{
+	}
 
 	Cell::Cell(
 		std::uint32_t definitionId,
@@ -33,5 +52,27 @@ namespace Voxel
 			definitionId |
 			((orientationValue << OrientationShift) & OrientationMask) |
 			((flipOrientationValue << FlipOrientationShift) & FlipOrientationMask);
+	}
+
+	std::uint32_t Cell::definitionId() const noexcept
+	{
+		return _packed & DefinitionMask;
+	}
+
+	Cell::Orientation Cell::orientation() const noexcept
+	{
+		return static_cast<Orientation>(
+			(_packed & OrientationMask) >> OrientationShift);
+	}
+
+	Cell::FlipOrientation Cell::flipOrientation() const noexcept
+	{
+		return static_cast<FlipOrientation>(
+			(_packed & FlipOrientationMask) >> FlipOrientationShift);
+	}
+
+	std::uint32_t Cell::packed() const noexcept
+	{
+		return _packed;
 	}
 }
