e2d2464301e462825612239fe3392d226165d5b5
Hyarius <hyarius@erelia-studio.com>
2026-09-24 09:06:25 +0200

    Assert Material SlotID contracts

--- core/tests/voxel_catalog_test.cpp
+++ core/tests/voxel_catalog_test.cpp
@@ -16,6 +16,9 @@ static_assert(std::is_copy_constructible_v<Voxel::Definition>);
 static_assert(std::is_move_constructible_v<Voxel::Definition>);
 static_assert(!std::is_copy_assignable_v<Voxel::Definition>);
 static_assert(!std::is_move_assignable_v<Voxel::Definition>);
+static_assert(std::is_same_v<Voxel::Material::SlotID, std::string>);
+static_assert(std::is_same_v<Voxel::Definition::SlotBindings::key_type, Voxel::Material::SlotID>);
+static_assert(std::is_same_v<decltype(Voxel::Shape::Polygon::slot), Voxel::Material::SlotID>);
 static_assert(std::is_convertible_v<Voxel::Shape::Catalog *, spk::JSON::Catalog<Voxel::Shape> *>);
 static_assert(std::is_convertible_v<Voxel::Definition::Catalog *, spk::JSON::Catalog<Voxel::Definition> *>);
 
