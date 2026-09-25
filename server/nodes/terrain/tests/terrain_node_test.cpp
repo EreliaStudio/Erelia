02f807e76d999df0319eed37cc1d2f19dbf3cb0c
Hyarius <hyarius@erelia-studio.com>
2026-09-25 09:43:11 +0200

    Test server node signal shutdown

--- server/nodes/terrain/tests/terrain_node_test.cpp
+++ server/nodes/terrain/tests/terrain_node_test.cpp
@@ -1,12 +1,19 @@
 #include "terrain_node.hpp"
+#include "terrain_node_application.hpp"
 
 #include <exception.hpp>
 #include <gtest/gtest.h>
 #include <type/uuid.hpp>
 
+#include <chrono>
+#include <csignal>
+#include <exception>
 #include <filesystem>
 #include <fstream>
 #include <string>
+#include <thread>
+
+using namespace std::chrono_literals;
 
 namespace
 {
@@ -39,6 +46,85 @@ namespace
 			return _path;
 		}
 	};
+
+	template <typename TPredicate>
+	[[nodiscard]] bool waitUntil(
+		TPredicate predicate,
+		std::chrono::milliseconds timeout = 2s)
+	{
+		const auto deadline =
+			std::chrono::steady_clock::now() + timeout;
+		while (std::chrono::steady_clock::now() < deadline)
+		{
+			if (predicate())
+			{
+				return true;
+			}
+			std::this_thread::sleep_for(5ms);
+		}
+		return predicate();
+	}
+
+	void expectApplicationStopsOnSignal(int signal)
+	{
+		TerrainNodeApplication application(
+			TerrainNode::Configuration{
+				.port = 0});
+
+		std::exception_ptr applicationFailure;
+		std::thread applicationThread(
+			[&]()
+			{
+				try
+				{
+					application.run();
+				} catch (...)
+				{
+					applicationFailure =
+						std::current_exception();
+				}
+			});
+
+		if (!waitUntil(
+				[&]()
+				{
+					return application.isRunning();
+				}))
+		{
+			application.stop();
+			applicationThread.join();
+			ADD_FAILURE()
+				<< "Terrain node application did not start before the deadline";
+			return;
+		}
+
+		if (std::raise(signal) != 0)
+		{
+			application.stop();
+			applicationThread.join();
+			ADD_FAILURE()
+				<< "Unable to raise signal "
+				<< signal;
+			return;
+		}
+
+		if (!waitUntil(
+				[&]()
+				{
+					return application.isRunning() == false;
+				}))
+		{
+			application.stop();
+			applicationThread.join();
+			ADD_FAILURE()
+				<< "Terrain node application did not stop after signal "
+				<< signal;
+			return;
+		}
+
+		applicationThread.join();
+		EXPECT_FALSE(applicationFailure);
+	}
 }
 
 TEST(TerrainNodeConfiguration, LoadsAndRejectsExactContract)
@@ -86,4 +172,14 @@ TEST(TerrainNodeRuntime, StartsStopsAndRestarts)
 	EXPECT_TRUE(node.isRunning());
 	EXPECT_NE(node.port(), 0u);
 	node.stop();
+}
+
+TEST(TerrainNodeApplication, StopsCleanlyOnInterruptSignal)
+{
+	expectApplicationStopsOnSignal(SIGINT);
+}
+
+TEST(TerrainNodeApplication, StopsCleanlyOnTerminationSignal)
+{
+	expectApplicationStopsOnSignal(SIGTERM);
 }
