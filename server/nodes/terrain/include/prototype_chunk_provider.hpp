6364cd28a647ee2068ccd7d57bc521c7a5d139ee
Hyarius <hyarius@erelia-studio.com>
2026-09-26 11:18:33 +0200

    refactor: simplify PrototypeChunkProvider contract

--- server/nodes/terrain/include/prototype_chunk_provider.hpp
+++ server/nodes/terrain/include/prototype_chunk_provider.hpp
@@ -1,43 +1,24 @@
 #pragma once
 
-#include <cstddef>
-#include <vector>
-
-#include <container/thread_safe_set.hpp>
+#include <threading/task.hpp>
 
 #include "erelia/core/chunk_collection.hpp"
-#include "erelia/core/task_group.hpp"
 
-class PrototypeChunkProvider final : public Chunk::Collection::Provider
+class PrototypeChunkProvider final :
+	public Chunk::Collection::Provider
 {
-private:
-	struct RequestHash
-	{
-		[[nodiscard]] std::size_t operator()(
-			const Chunk::Collection::Request &request) const noexcept;
-	};
-
-	using RequestSet = spk::ThreadSafeSet<
-		Chunk::Collection::Request,
-		RequestHash>;
-
-	struct PendingTaskGroup
-	{
-		std::vector<Chunk::Collection::Request> requests;
-		spk::TaskGroup<Chunk>::Answer answer;
-	};
-
-	RequestSet _requested;
-	std::vector<PendingTaskGroup> _pending;
-
 public:
 	PrototypeChunkProvider() = default;
-	PrototypeChunkProvider(const PrototypeChunkProvider &) = delete;
-	PrototypeChunkProvider(PrototypeChunkProvider &&) noexcept = default;
+	PrototypeChunkProvider(
+		const PrototypeChunkProvider &) = delete;
+	PrototypeChunkProvider(
+		PrototypeChunkProvider &&) noexcept = default;
 
-	PrototypeChunkProvider &operator=(const PrototypeChunkProvider &) = delete;
-	PrototypeChunkProvider &operator=(PrototypeChunkProvider &&) noexcept = default;
+	PrototypeChunkProvider &operator=(
+		const PrototypeChunkProvider &) = delete;
+	PrototypeChunkProvider &operator=(
+		PrototypeChunkProvider &&) noexcept = default;
 
-	void request(const Chunk::Collection::Request &request) override;
-	void update(Chunk::Collection &collection) override;
+	[[nodiscard]] spk::Task<Chunk>::Answer request(
+		const Chunk::Coordinate &coordinate) override;
 };
