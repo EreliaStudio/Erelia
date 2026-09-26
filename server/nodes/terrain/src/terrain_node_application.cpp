79692bb3fa3bd5e25f638579db14f313eefd8f78
Hyarius <hyarius@erelia-studio.com>
2026-09-26 11:14:34 +0200

    refactor: use Sparkle system ArgumentParser

--- server/nodes/terrain/src/terrain_node_application.cpp
+++ server/nodes/terrain/src/terrain_node_application.cpp
@@ -1,6 +1,6 @@
 #include "terrain_node_application.hpp"
 
-#include <structure/system/argument_parser.hpp>
+#include <system/argument_parser.hpp>
 
 #include <design_pattern/singleton.hpp>
 #include <diagnostics/logger.hpp>
